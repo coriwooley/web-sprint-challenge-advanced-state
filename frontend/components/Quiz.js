4a3392075f7ebbecc8ce0d122d462a5d7ee4876f
Cori Wooley <wooleycori@gmail.com>
2022-04-22 01:14:20 -0800

    Quiz.js complete

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { connect } from 'react-redux'
 import * as actionCreators from '../state/action-creators'
 
 export function Quiz(props) {
   const {quiz, fetchQuiz, postAnswer, selectedAnswer, selectAnswer} = props
+
+  useEffect(() => {
+    fetchQuiz()
+  }, [])
+
+  const handleClick = (id) => {
+    selectAnswer(id)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    postAnswer({quiz_id : quiz.quiz_id, answer_id: selectedAnswer})
+  }
   return (
     <div id="wrapper">
       {
@@ -15,20 +28,20 @@ export function Quiz(props) {
             <div id="quizAnswers">
               <div className={`${selectAnswer === quiz.answers[0].answer_id ? 'answer selected' : 'answer'}`}>
                 {quiz.answers[0].text}
-                <button>
-                  SELECTED
+                <button onClick={() => handleClick(quiz.answers[0].answer_id)}>
+                  {selectedAnswer === quiz.answers[0].answer_id ? 'SELECTED' : 'Select'}
                 </button>
               </div>
 
-              <div className="answer">
-                An elephant
-                <button>
-                  Select
+              <div className={`${selectAnswer === quiz.answers[1].answer_id ? 'answer selected' : 'answer'}`}>
+                {quiz.answers[1].text}
+                <button onClick={() => handleClick(quiz.answers[1].answer_id)}>
+                  {selectedAnswer === quiz.answers[1].answer_id ? 'SELECTED' : 'Select'}
                 </button>
               </div>
             </div>
 
-            <button id="submitAnswerBtn">Submit answer</button>
+            <button id="submitAnswerBtn" disabled={!selectedAnswer} onClick={handleSubmit}>Submit answer</button>
           </>
         ) : 'Loading next quiz...'
       }
